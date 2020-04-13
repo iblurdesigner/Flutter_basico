0cc033cd615ac82ebad13419ae8d49d605a8ab4b
iblurdesigner <nevomen@gmail.com>
2020-04-12 20:01:11 -0500

    Fin del curso basico

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'description_place.dart';
-import 'review_list.dart';
-import 'gradient_back.dart';
 import 'package:flutter/services.dart';
-import 'header_appbar.dart';
+import 'platzi_trips.dart';
+import 'platzi_trips_cupertino.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  final descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \n\nIt was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
   @override
   Widget build(BuildContext context) {
@@ -31,23 +28,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-//        appBar: AppBar(
-//          title: Text("Platzi Turism"),
-//        ),
-        // body: DescriptionPlace("Bahamas", 5, descriptionDummy),
-        body: Stack(
-          children: <Widget>[
-            ListView(
-              children: <Widget>[
-                DescriptionPlace("Bahamas", 5, descriptionDummy),
-                ReviewList()
-              ],
-            ),
-            HeaderAppBar()
-          ],
-        ),
-      ) //MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PlatziTrips() //MyHomePage(title: 'Flutter Demo Home Page'),
+//      home: PlatziTripsCupertino() //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
